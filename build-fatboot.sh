6bf2169edf0daf3c3f2a6c8676f306d027b8693b
george <george@cool-pi.com>
2023-07-24 11:17:22 +0800

    add cm5-noteboot config

--- build-fatboot.sh
+++ build-fatboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Create coolpi boot img..."
-dd if=/dev/zero of=coolpi-boot.img bs=1M count=300
+dd if=/dev/zero of=coolpi-boot.img bs=1M count=500
 mkfs.vfat -F 32 coolpi-boot.img
 fatlabel coolpi-boot.img system-boot
 mkdir -p .temp
