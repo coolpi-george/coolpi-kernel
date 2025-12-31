c945e472e6be85472fc3a94d6c079d39585dbb5f
george <george@cool-pi.com>
2025-12-31 13:43:45 +0800

    add build-fatboot.sh

---
+++ build-fatboot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Create coolpi boot img..."
+dd if=/dev/zero of=coolpi-boot.img bs=1M count=500
+mkfs.vfat -F 32 coolpi-boot.img
+fatlabel coolpi-boot.img boot
+mkdir -p .temp
+mount coolpi-boot.img .temp/
+cp -r out/* .temp/
+umount .temp
+echo "Complete..."
