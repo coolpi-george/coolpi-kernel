bf64834867e6b1a2be61e5927569c0f89fab17ac
george <george@cool-pi.com>
2026-08-07 17:34:32 +0800

    focaltech_touch_ft8201p remove fw_sample.i

--- drivers/input/touchscreen/focaltech_touch_ft8201p/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch_ft8201p/focaltech_config.h
@@ -343,19 +343,19 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_MODULE_ID
  */
-#define FTS_UPGRADE_FW_FILE                     "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_FW_FILE                     "include/firmware/fw_sample.h"
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_MODULE2_ID
  * define your own fw_file, the sample one is invalid
  */
-#define FTS_UPGRADE_FW2_FILE                    "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_FW2_FILE                    "include/firmware/fw_sample.h"
 
 /*
  * if FTS_GET_MODULE_NUM >= 3, fw corrsponding with FTS_MODULE3_ID
  * define your own fw_file, the sample one is invalid
  */
-#define FTS_UPGRADE_FW3_FILE                    "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_FW3_FILE                    "include/firmware/fw_sample.h"
 
 /*********************************************************/
 
